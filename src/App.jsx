888f69968d9ee2a3a400d37e99017503e416aea6
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2022-06-07 21:59:24 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ export default class App extends Component {
     return (
       <main>
         <h1>Hello World from React! 📦 🚀</h1>
+        <p>This domain is for use in illustrative examples in documents. You may use this
+        domain in literature without prior coordination or asking for permission.</p>
+        <p><a href="https://www.iana.org/domains/example">More information...</a></p>
       </main>
     )
   }
