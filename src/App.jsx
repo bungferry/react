97d1990637750f366da476f57a6180241d2a6f66
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2022-06-09 13:42:53 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ export default class App extends Component {
         LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
         OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
         SOFTWARE.</p>
-        <img src="https://raw.githubusercontent.com/bungferry/react/main/src/autor.png"/>
+        <img src="https://raw.githubusercontent.com/bungferry/react/main/src/autor.png" width="50" height="50">
       </main>
     )
   }
